b60846bfd136ddae10b51b32b34036553b9baa31
mankudinesh <mankudinesh2005@gmail.com>
2026-03-15 21:58:10 +0530

    Add files via upload

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,50 @@
+const todoList = [];
+
+renderTodoList();
+
+function renderTodoList()
+{
+let todoListHTML = '';
+for(let i=0;i<todoList.length;i++)
+{
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+    const html = `<div>${name}</div>
+    <div>${dueDate}</div>
+     <button onclick="
+     todoList.splice(${i}, 1);
+     renderTodoList();
+     " class="delete-button">
+     Delete
+     </button>`;
+    todoListHTML += html;
+}
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function myOwn(event)
+{
+    if(event.key === 'Enter')
+        {
+            addTodo();
+        }
+}
+function addTodo()
+{
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dueDateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dueDateInputElement.value;
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate
+        name,
+        dueDate
+    });
+    
+    console.log(todoList);
+    inputElement.value = '';
+    renderTodoList();
+}
